f9c5870a9849eb17a0aa1aa9a0632e36802d8bf1
mimaraslan <mimaraslan@gmail.com>
2025-10-05 11:14:48 +0300

    Frontend React

--- devops-terraform/01_JenkinsServer/03_install.sh
+++ devops-terraform/01_JenkinsServer/03_install.sh
@@ -61,6 +61,8 @@ sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
 
+
+#eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 cd /tmp
 sudo mv /tmp/eksctl /bin
